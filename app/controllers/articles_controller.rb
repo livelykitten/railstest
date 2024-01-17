e8abfb6736a3ec33fd3091fda53a85f208e0d88f
Livelykitten <ichupu9999@gmail.com>
2024-01-17 19:30:41 +0900

    Revert "devise"
    This reverts commit 9227b06655a351db71e8e3069f2332c1d5345ee2.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,4 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
   def index
-    @articles = Article.all
-  end
-  def show
-    @article = Article.find(params[:id])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,4 @@
 class Article < ApplicationRecord
-    
-
     def change
         create_table :articles do |t|
             t.string :title
